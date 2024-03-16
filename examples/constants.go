61bb2b20013befae7fc5ba062b9e18c797d208d0
adityjha0 <aditya2005aps@gmail.com>
2024-03-16 22:29:00 +0530

    initial commit

---
+++ examples/constants.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// global
+const s = "string"
+
+func main(){
+	fmt.Println(s)
+
+	const n = 100
+
+    const d = 3e20 / n
+    fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(d))
+}
